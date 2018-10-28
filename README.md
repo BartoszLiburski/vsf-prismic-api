510542b4dad28f98117d4570f4de866e6c2edcba
Bartosz Liburski <bartosz.liburski@gmail.com>
2018-10-28 15:54:57 +0100

    new vsf core, getting content by id

--- README.md
+++ README.md
@@ -2,20 +2,26 @@
 
 This API extension get data from prismic repository  
 
-In your `local.json` file you should register the extension:
+# Instalation
+In your `config/local.json` file you should register the extension:
 `"registeredExtensions": ["cms-prismic"],`
 
 and also add your prismic API endpoint: 
 
-  `"prismic": {
+  `
+  "prismic": {
     "apiEndpoint": "https://publicrepo.prismic.io/api/v2"
-  }`
+  }
+  `
 
 Make sure that you have installed `prismic-javascript` npm module 
 `npm install prismic-javascript --save`
 
+# Info
+
 The API endpoitns are:
 ```
 /api/ext/cms-data/cmsPrismic/:type/:orderings
+/api/ext/cms-data/cmsPrismic/:id
 ```
 
--- index.js
+++ index.js
@@ -12,9 +12,19 @@ module.exports = ({ config }) => {
       return api.query(Prismic.Predicates.at('document.type', req.params.type),
         {orderings: `[$req.params.orderings]`});
     }).then(response => {
-      console.log(response.results)
       apiStatus(res, response.results, 200);
-    }, function (err) {
+    }, (err) => {
+      console.log("Something went wrong: ", err);
+    }).catch(err => {
+      apiStatus(res, err, 500);
+    });
+  });
+  cmsApi.get('/cmsPrismic/:id', (req, res) => {
+    return Prismic.getApi(config.prismic.apiEndpoint, {req: req}).then(function (api) {
+      return api.query(Prismic.Predicates.at('document.id', req.params.id));
+    }).then(response => {
+      apiStatus(res, response.results, 200);
+    }, (err) => {
       console.log("Something went wrong: ", err);
     }).catch(err => {
       apiStatus(res, err, 500);
